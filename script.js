3e7463d6841a8299658e887fed004de3ec5d541e
NickUzun <134712718+NickUzun@users.noreply.github.com>
2025-03-30 22:46:53 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function toggleSidebar() {
+    document.getElementById("sidebar").classList.toggle("active");
+}
+
+function toggleDropdown(element) {
+    element.classList.toggle("active");
+}
+
+
